8e50412b76d0c25386d53dfb7e01d548f9c6aacf
Mu'ayad Al Shareef <103497877+muayedjj@users.noreply.github.com>
2022-07-04 22:41:28 +0300

    dunno-what-this-is

---
+++ API/capital_finder.py
@@ -0,0 +1,32 @@
+from http.server import BaseHTTPRequestHandler
+from urllib import parse
+import requests
+
+
+class Handler(BaseHTTPRequestHandler):
+    def do_get(self):
+        path = self.path
+        url_components = parse.urlsplit(path)
+        query_string_list = parse.parse_qsl(url_components.query)
+        dic = dict(query_string_list)
+        definitions = []
+        if 'capital' in dic:
+            capital = dic['capital']
+            url = f'https://restcountries.com/v3.1/capital/{capital}'
+            req = requests.get(url)
+            data = req.json()
+            for capital_data in data:
+                definition = capital_data['meanings'][0]['definitions'][0]['definition']
+                definitions.append(definition)
+
+            message = str(definitions)
+
+        else:
+            message = "Please provide me with a word"
+
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(message.encode())
+
+        return
