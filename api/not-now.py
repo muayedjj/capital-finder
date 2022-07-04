8c4037c375cf70e67b46e4ff93dbb6a5434f3beb
Mu'ayad Al Shareef <103497877+muayedjj@users.noreply.github.com>
2022-07-04 23:44:12 +0300

    dunno-what-this-is-04

--- api/not-now.py
+++ api/not-now.py
@@ -9,20 +9,20 @@ class handler(BaseHTTPRequestHandler):
         url_components = parse.urlsplit(s)
         query_string_list = parse.parse_qsl(url_components.query)
         dic = dict(query_string_list)
-        definitions = []
+        cap = ''
         if 'word' in dic:
             word = dic['word']
             url = 'https://restcountries.com/v3.1/capital/'
             r = requests.get(url + word)
             data = r.json()
             for word_data in data:
-                definition = word_data['meanings'][0]['definitions'][0]['definition']
-                definitions.append(definition)
+                definition = word_data[0]['name']['common']
+                cap += definition
 
-            message = str(definitions)
+            message = str(cap)
 
         else:
-            message = "Please provide me with a word"
+            message = "Please provide me with a country or a capital"
 
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
